f5dcd568b12025a5b0cfe19e831b4d97c6b4364e
Enver Tuna Sönmez <etsonmez01@gmail.com>
2024-07-15 00:16:19 +0300

    the first step
    don't take care to this commit pls. i try something

---
+++ lib/widgets/nav_bar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class NavBarWidget extends StatelessWidget {
+  const NavBarWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row();
+  }
+}
